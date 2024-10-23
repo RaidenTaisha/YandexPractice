60ed1b2bf41dcc9581dcb30fb608dc0c9fbceea7
Мяделец Андрей Алексеевич <andreymydelec@gmail.com>
2024-10-23 15:47:58 +0700

    code review 8.1: математические постоянные значения, заменены на константы

--- Спринт 8/Тема 2 Имена и пространства имён/Урок 8.3/geo.h
+++ Спринт 8/Тема 2 Имена и пространства имён/Урок 8.3/geo.h
@@ -18,8 +18,9 @@ inline double ComputeDistance(Coordinates from, Coordinates to) {
   if (from == to) {
     return 0;
   }
-  static const double dr = 3.1415926535 / 180.;
-  return acos(
-      sin(from.lat * dr) * sin(to.lat * dr) + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-      * 6371000;
+  static const double rad = M_PI / 180.;
+  static const double earth_radius  = 6371000;
+  return acos(sin(from.lat * rad) * sin(to.lat * rad)
+    + cos(from.lat * rad) * cos(to.lat * rad)
+      * cos(abs(from.lng - to.lng) * rad)) * earth_radius;
 }
