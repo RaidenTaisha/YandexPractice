RaidenTaisha/YandexPractice

commit 3185e079b43db59becd781b5440a57f2d50ef343

code review 8.1: метод чтения из входного потока инкапсулирован в класс Reader

Спринт 8/Тема 2 Имена и пространства имён/Урок 8.3/input_reader.cpp
@@ -3,8 +3,8 @@
 #include "transport_catalogue.h"
 
 #include <algorithm>
-#include <cassert>
-#include <iterator>
+#include <string>
+#include <istream>
 
 /**
 * Парсит строку вида "10.123,  -30.1837" и возвращает пару координат (широта, долгота)
@@ -116,4 +116,12 @@ void InputReader::ApplyCommands(tc::TransportCatalogue& catalogue) const {
       catalogue.AddStop(command.id, ParseCoordinates(command.description));
     else if (command.command == "Bus")
       catalogue.AddRoute(command.id, ParseRoute(command.description));
+}
+
+void InputReader::ReadCommands(std::istream& is, size_t count) {
+  for (size_t i = 0; i < count; ++i) {
+    std::string line;
+    getline(is, line);
+    ParseLine(line);
+  }
 }

Спринт 8/Тема 2 Имена и пространства имён/Урок 8.3/input_reader.h
@@ -33,6 +33,11 @@ class InputReader {
 */
   void ApplyCommands(tc::TransportCatalogue& catalogue) const;
 
+/**
+* Парсит count строк из потока ввода в структуру CommandDescription и сохраняет результат в commands_
+*/
+  void ReadCommands(std::istream& is, size_t count);
+
  private:
   std::vector<CommandDescription> commands_;
 };

Спринт 8/Тема 2 Имена и пространства имён/Урок 8.3/main.cpp
@@ -15,11 +15,7 @@ int main() {
 
   {
     InputReader reader;
-    for (int i = 0; i < base_request_count; ++i) {
-      string line;
-      getline(cin, line);
-      reader.ParseLine(line);
-    }
+    reader.ReadCommands(cin, base_request_count);
     reader.ApplyCommands(catalogue);
   }
 

commit 2dec1b0dc08fea3ca9db968de3aa727148a60f97

code review 8.2: считывание количества операций также вынесено в из main
Fixes: 3185e079("code review 8.1: метод чтения из входного потока инкапсулирован
в класс Reader")
Fixes: fa95e3e1("code review 8.1: запроса данных из справочника вынесен в файл
stat_reader")

Спринт 8/Тема 2 Имена и пространства имён/Урок 8.3/input_reader.cpp
@@ -118,7 +118,9 @@ void InputReader::ApplyCommands(tc::TransportCatalogue& catalogue) const {
       catalogue.AddRoute(command.id, ParseRoute(command.description));
 }
 
-void InputReader::ReadCommands(std::istream& is, size_t count) {
+void InputReader::ReadCommands(std::istream& is) {
+  size_t count;
+  is >> count >> std::ws;
   for (size_t i = 0; i < count; ++i) {
     std::string line;
     getline(is, line);

Спринт 8/Тема 2 Имена и пространства имён/Урок 8.3/input_reader.h
@@ -34,9 +34,9 @@ class InputReader {
   void ApplyCommands(tc::TransportCatalogue& catalogue) const;
 
 /**
-* Парсит count строк из потока ввода в структуру CommandDescription и сохраняет результат в commands_
+* Парсит строки из потока ввода в структуру CommandDescription и сохраняет результат в commands_
 */
-  void ReadCommands(std::istream& is, size_t count);
+  void ReadCommands(std::istream& is);
 
  private:
   std::vector<CommandDescription> commands_;

Спринт 8/Тема 2 Имена и пространства имён/Урок 8.3/main.cpp
@@ -9,16 +9,11 @@ using namespace tc;
 int main() {
   TransportCatalogue catalogue;
 
-  int base_request_count;
-  cin >> base_request_count >> ws;
-
   {
     InputReader reader;
-    reader.ReadCommands(cin, base_request_count);
+    reader.ReadCommands(cin);
     reader.ApplyCommands(catalogue);
   }
 
-  int stat_request_count;
-  cin >> stat_request_count >> ws;
-  ParseCommands(catalogue, cin, stat_request_count);
+  ParseCommands(catalogue, cin);
 }

Спринт 8/Тема 2 Имена и пространства имён/Урок 8.3/stat_reader.cpp
@@ -45,7 +45,9 @@ void ParseAndPrintStat(const TransportCatalogue& catalogue, std::string_view req
     PrintStopStat(catalogue, id, output);
 }
 
-void ParseCommands(const tc::TransportCatalogue& catalogue, std::istream& is, size_t count) {
+void ParseCommands(const tc::TransportCatalogue& catalogue, std::istream& is) {
+  size_t count;
+  is >> count >> std::ws;
   for (size_t i = 0; i < count; ++i) {
     std::string line;
     getline(is, line);

Спринт 8/Тема 2 Имена и пространства имён/Урок 8.3/stat_reader.h
@@ -7,4 +7,4 @@
 
 void ParseAndPrintStat(const tc::TransportCatalogue& catalogue, std::string_view request, std::ostream& output);
 
-void ParseCommands(const tc::TransportCatalogue& catalogue, std::istream& is, size_t count);
+void ParseCommands(const tc::TransportCatalogue& catalogue, std::istream& is);